1ddc032d54ce9512f5a88e97c28e97a1d21101a9
Haley <haley@121nexus.com>
2016-11-04 17:21:30 -0400

    Unit tests run for all reducers.

--- src/Reducers.js
+++ src/Reducers.js
@@ -3,30 +3,31 @@ import Redux, { createStore, combineReducers } from 'redux';
 // Reducers
 
 const addInputR = (state, action) => {
-  new_input = {
+  const new_input = {
+    id: action.id,
     name: action.name ? action.name : '',
     total: action.total ? action.total : 0
   };
   if (action.def) {new_input.def = action.def};
   if (action.notes) {new_input.notes = action.notes};
-  new_state = state.concat(new_input);
+  const new_state = state.concat(new_input);
   return new_state;
 }
 
 const editInputR = (state, action) => {
-  new_input = {
-    name: action.name ? action.name : '',
-    total: action.total ? action.total : 0
+  const new_input = {
+    name: action.name ? action.name : state.name ? state.name : '',
+    total: action.total ? action.total : state.total ? state.total : 0
   };
   if (action.def) {new_input.def = action.def};
   if (action.notes) {new_input.notes = action.notes};
-  new_state = Object.assign ({}, state, {new_input});
+  const new_state = Object.assign ({}, state, new_input);
   return new_state;
 }
 
 const name = (state='', action) => {
   switch (action.type) {
-    case ('EDIT_NAME'): return action.text
+    case ('EDIT_NAME'): return action.name
   default:
     return state;
   }
@@ -35,13 +36,13 @@ const name = (state='', action) => {
 const threat = (state={threat:{},hardness:{},armor:{}}, action) => {
   switch (action.type) {
     case ('EDIT_THREAT'):
-      newThreat = editInputR (state.threat, action);
+      const newThreat = editInputR (state.threat, action);
       return Object.assign ({}, state, {threat: newThreat});
     case ('EDIT_HARDNESS'):
-        newHard = Object.editInputR (state.hardness, action);
-        return Object.assign ({}, state, {hardness: newHardness});
+        const newHard = editInputR (state.hardness, action);
+        return Object.assign ({}, state, {hardness: newHard});
     case ('EDIT_ARMOR'):
-        newArmor = Object.assign ({}, state.armor, {total: action.total, notes: action.text})
+        const newArmor = editInputR (state.armor, action);
         return Object.assign ({}, state, {armor: newArmor})
   default:
     return state;
@@ -86,11 +87,11 @@ const resources = (state=[], action) => {
 
 const powers = (state=[], action) => {
   switch (action.type) {
-    case ('ADD_POWER'): return state.concat([{name: '', description: ''}])
+    case ('ADD_POWER'): return state.concat([{id: action.id, name: '', description: ''}])
     case ('EDIT_POWER'):
       return state.map( p => {
           if (action.id == p.id) {
-            new_p = Object.assign ({}, p, {name: action.name, name:action.description});
+            const new_p = Object.assign ({}, p, {name: action.name, description:action.description});
             return new_p};
       });
     default:
--- src/Reducers.test.js
+++ src/Reducers.test.js
@@ -3,10 +3,75 @@ import ReactDOM from 'react-dom';
 import Redux, { createStore, combineReducers } from 'redux';
 import reducers from './Reducers';
 
-const empty_state = {};
-const empty_action = {};
+const empty_state = {
+  name: '',
+  threat: {"armor": {},
+          "hardness": {},
+          "threat": {},},
+  virtues: [],
+  skills: [],
+  resources: [],
+  powers: [],
+};
 
-it('create store runs', () => {
+it('create store generates empty state', () => {
   const store = createStore(reducers);
-  store.subscribe (() => {console.log(store.getState())})
+  expect(store.getState()).toEqual(empty_state);
 });
+
+it('unknown action returns default state', () => {
+  const store = createStore(reducers);
+  expect(reducers(store.getState(), "UNKNOWN_ACTION")).toEqual(empty_state);
+})
+
+it('EDIT_NAME updates name', () => {
+  const store = createStore(reducers);
+  store.dispatch({type: "EDIT_NAME", name:"Shula Al'Aquil"});
+  expect(store.getState().name).toEqual("Shula Al'Aquil");
+})
+
+it('threat actions update threat', () => {
+  const store = createStore(reducers);
+  store.dispatch({type: "EDIT_THREAT", name:"", total:4})
+  expect(store.getState().threat.threat).toEqual({name:"" , total:4})
+  store.dispatch({type: "EDIT_ARMOR", name:"", total:3, notes:"v fire"})
+  expect(store.getState().threat.armor).toEqual({name:"" , total:3, notes:"v fire"})
+  store.dispatch({type: "EDIT_HARDNESS", name:"", total:1, notes:"when in water"})
+  expect(store.getState().threat.hardness).toEqual({name:"", total:1, notes:"when in water"})
+})
+
+it('virtue actions update virtues', () => {
+  const store = createStore(reducers);
+  store.dispatch({type: "ADD_VIRTUE", name:"Hero", id:1, total:3});
+  expect(store.getState().virtues[0]).toEqual({name:"Hero", id:1, total:3});
+  store.dispatch({type: "EDIT_VIRTUE", name:"Hero", id:1, total:5});
+  expect(store.getState().virtues[0]).toEqual({name:"Hero", id:1, total:5});
+})
+
+it('skill actions update skills', () => {
+  const store = createStore(reducers);
+  store.dispatch({type: "ADD_SKILL", name:"Scamper", id:2, total:5});
+  expect(store.getState().skills[0]).toEqual({name:"Scamper", id:2, total:5});
+  store.dispatch({type: "EDIT_SKILL", name:"Scamp", id:2, total:4});
+  expect(store.getState().skills.length).toEqual(1);
+  expect(store.getState().skills[0]).toEqual({name:"Scamp", id:2, total:4});
+  store.dispatch({type: "ADD_SKILL", id:2});
+  expect(store.getState().skills.length).toEqual(2);
+})
+
+it('resource actions update custom resources', () => {
+  const store = createStore(reducers);
+  store.dispatch({type: "ADD_RESOURCE", name:"Fire", id:3, total:5, def:1, notes:"See powers"});
+  expect(store.getState().resources[0]).toEqual({name:"Fire", id:3, total:5, def:1, notes:"See powers"});
+  store.dispatch({type: "EDIT_RESOURCE", id:3, total:4});
+  expect(store.getState().resources.length).toEqual(1);
+  expect(store.getState().resources[0]).toEqual({name:"Fire", id:3, total:4, def:1, notes:"See powers"});
+})
+
+it('power actions update powers', () => {
+  const store = createStore(reducers);
+  store.dispatch({type: "ADD_POWER", id:5});
+  expect(store.getState().powers.length).toEqual(1);
+  store.dispatch({type:"EDIT_POWER", id:5, name:"Shape the Islands", description:"Spend 1 fire to take twenty on a wield your powers roll"});
+  expect(store.getState().powers[0]).toEqual({id:5, name:"Shape the Islands", description:"Spend 1 fire to take twenty on a wield your powers roll"});
+})
